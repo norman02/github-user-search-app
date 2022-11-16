beb7df07742c7f3eb70bcc0f74cc87208ad96a69
John N. Norman <normanj777@gmail.com>
2022-11-16 10:22:29 -0500

    define global layout

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import './scss/main.scss'
 export default function App() {
   return (
     <main className="App">
+      <div className="container p-3">
+
       <header>
         <h1>devfinder</h1>
         <Toggle />
       </header>
       <SearchBar />
       <ProfileCard />
+      </div>
     </main>
   );
 }
